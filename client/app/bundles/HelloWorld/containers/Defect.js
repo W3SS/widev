6374a3d532ec88de7ee0bc7902538c80dc071157
andrea.terzani <Luglio2015>
2016-09-20 16:16:42 +0200

    VFall

--- client/app/bundles/HelloWorld/containers/Defect.js
+++ client/app/bundles/HelloWorld/containers/Defect.js
@@ -6,7 +6,7 @@ import Axios from 'axios';
 export default class Defect extends React.Component {
  static propTypes = {
     currentUser: PropTypes.object.isRequired, // this is passed from the Rails view
-     rmitems: PropTypes.object.isRequired,
+     rmitems: PropTypes.array.isRequired,
      installation: PropTypes.object
  };
   
@@ -21,13 +21,33 @@ export default class Defect extends React.Component {
 
     handleVF(i){
         console.log("Setting Validate FIX on : " + i);
-        console.log(i);
         Axios.get('/api/rtc/updateWorkItemByInstallation?wi='+i.DESCRIPTION+'&installation_id='+this.props.installation.id).then( (response) =>{
 
         })
         .catch(function (error) {
             alert(error);
         });
+    }
+
+    handleVFAll(){
+
+        let alldefects = [];
+        this.state.rmitems.forEach((i)=>{
+            if (!isNaN(i.DESCRIPTION) && alldefects.indexOf(i.DESCRIPTION)== -1){
+                alldefects.push(i.DESCRIPTION)
+            }
+        });
+
+        alldefects.forEach((wi)=>{
+            Axios.get('/api/rtc/updateWorkItemByInstallation?wi='+wi+'&installation_id='+this.props.installation.id).then( (response) =>{
+
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+         });
+
+        console.log(alldefects);
     }
 
   render() {
@@ -37,13 +57,11 @@ export default class Defect extends React.Component {
           let btn="";
           if (!isNaN(i.DESCRIPTION)){
               btn = <button onClick={ () => this.handleVF(i) } className="btn btn-xs btn-success">Validate FIX</button>
-              //btn = <button className="btn btn-sm btn-success">Validate FIX</button>
           }else{
               btn = "No action"
           }
-
           return(
-              <tr>
+              <tr key={i.NAME}>
                   <td>{i.NAME}</td>
                   <td>{i.DESCRIPTION}</td>
                   <td>{i.TYPE}</td>
@@ -55,13 +73,14 @@ export default class Defect extends React.Component {
 
       return(
           <div>
+
               <table className="table table-bordered objtable">
                   <thead>
                   <tr>
                       <th>Object</th>
-                      <th>Description</th>
+                      <th>Description </th>
                       <th>Type</th>
-                      <th>RTC</th>
+                      <th>RTC <button className="btn btn-xs btn-danger" onClick= {()=>this.handleVFAll()} > Validate FIX All </button> </th>
                   </tr>
                   </thead>
                   <tbody>
